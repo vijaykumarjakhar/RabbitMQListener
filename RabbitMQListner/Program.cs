1cdead25cd1b155854dc48e8b5dd2e7630eaa541
vijaykumarjakhar <vijayjakhar2010@gmail.com>
2020-12-13 13:05:39 +0530

    RabbitMQListener add for asp.net C#

---
+++ RabbitMQListner/Program.cs
@@ -0,0 +1,47 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+using System.Threading;
+
+namespace RabbitMQListner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            string queueName = "demoQueue";
+            var rabbitMqConnection = factory.CreateConnection();
+            var rabbitMqChannel = rabbitMqConnection.CreateModel();
+
+            rabbitMqChannel.QueueDeclare(queue: queueName,
+                                 durable: false,
+                                 exclusive: false,
+                                 autoDelete: false,
+                                 arguments: null);
+
+            rabbitMqChannel.BasicQos(prefetchSize: 0, prefetchCount: 1, global: false);
+
+            int messageCount = Convert.ToInt16(rabbitMqChannel.MessageCount(queueName));
+            Console.WriteLine(" Listening to the queue. This channels has {0} messages on the queue", messageCount);
+
+            var consumer = new EventingBasicConsumer(rabbitMqChannel);
+            consumer.Received += (model, ea) =>
+            {
+                var body = ea.Body;
+                var message = Encoding.UTF8.GetString(body);
+                Console.WriteLine(" Document received: " + message);
+                rabbitMqChannel.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false);
+                Thread.Sleep(1000);
+            };
+            rabbitMqChannel.BasicConsume(queue: queueName,
+                                 autoAck: false,
+                                 consumer: consumer);
+
+            Thread.Sleep(1000 * messageCount);
+            Console.WriteLine(" Connection closed, no more messages.");
+            Console.ReadLine();
+        }
+    }
+}
